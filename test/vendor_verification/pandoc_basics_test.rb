56e3e1bfdeb01e41696bded9c2c07dee6d71a5d8
Jeff Dickey <jdickey@seven-sigma.com>
2016-04-09 20:48:28 +0800

    "Pandoc basics" verification spec green on both OS X and Linux.
    Now to fix how `replace_content` uses it; it still fails on Linux only.
    
    26 tests, 16 assertions, 0 failures, 0 errors, 15 skips
    Coverage: 196/196 LOC (100.0%) covered.
    RuboCop: 9 files inspected, no offenses detected
    Flay: Total score 0
    Flog: Total 146.8; method average 4.9; max 13.9 (Prolog::Services::ReplaceContent::ContentReplacer#tail)
    Reek: 0 warnings

--- test/vendor_verification/pandoc_basics_test.rb
+++ test/vendor_verification/pandoc_basics_test.rb
@@ -1,30 +1,19 @@
 # frozen_string_literal: true
 
 require 'test_helper'
-require 'semantic_logger'
+require 'rbconfig'
 
 require 'pandoc-ruby'
 
-describe 'Pandoc basics' do
-  before do
-    SemanticLogger.default_level = :trace
-    @logger = SemanticLogger['PandocBasics']
-  end
+is_linux = RbConfig::CONFIG['host_os'] == 'linux-gnu'
+is_osx = RbConfig::CONFIG['host_os'].match(/^darwin/)
 
-  tag :focus
-  describe 'converts empty anchor tag pairs with IDs to span tag pairs' do
+describe 'Pandoc basics' do
+  describe 'has OS-dependent parsing of identified anchor pairs' do
     let(:html_content) do
       content = <<~ENDIT
-      <p>This is
-       <a id="contribution-27-begin"></a>
-      <a id="contribution-9-begin"></a>
-      obviously
-       <em>sample
-       <a id="contribution-27-end"></a>
-       and disposable
-      </em>
-       content.
-      <a id="contribution-9-end"></a>
+      <p>This is <a id="contrib-99"></a>
+      obviously <em>sample and disposable</em> content.
       </p>
       ENDIT
       content.lines.map(&:chomp).join
@@ -33,43 +22,55 @@ describe 'Pandoc basics' do
       PandocRuby.convert(html_content, from: :html, to: :markdown_github)
     end
     let(:converted_html) do
-      # Pandoc adds a aterminating "\n"; kill it."
+      # Pandoc adds a aterminating "\n"; kill it.
       PandocRuby.convert(markdown, from: :markdown_github, to: :html).chomp
     end
-
-    it 'does not produce lexically identical content' do
-      expect(converted_html).wont_equal html_content
+    # let(:fiddled_content) do
+    #   html_content.sub('<a id="contrib-99"></a>', 'QQ99QQ')
+    # end
+    let(:fiddled_markdown) do
+      md = PandocRuby.convert(html_content, from: :html, to: :markdown_github)
+      md.sub('QQ99QQ', '<a id="contrib-99"></a>')
+    end
+    let(:fiddled_html) do
+      PandocRuby.convert(fiddled_markdown, from: :markdown_github,
+                                           to: :html).chomp
     end
 
-    # Changed sometime between Pandoc 1.15.1 and 1.17.0.3.
-    it 'adds empty :href attributes to empty :a tags' do
-      fiddled = html_content.gsub('<a id', '<a href="" id')
-      @logger.trace 'adds empty :href attribute', fiddled: fiddled,
-                                                  markdown: markdown,
-                                                  html_content: html_content,
-                                                  converted_html: converted_html
-      expect(converted_html).must_equal fiddled
-    end
-  end # describe 'converts empty anchor tag pairs with IDs to span tag pairs'
+    describe 'on OS X but not on Linux' do
+      # Changed sometime between Pandoc 1.15.1 and 1.17.0.3.
+      it 'adds empty :href attributes to empty :a tags' do
+        fiddled = html_content.gsub('<a id', '<a href="" id')
+        expect(converted_html).must_equal fiddled if is_osx
+      end
+    end # describe 'on OS X but not on Linux'
 
-  tag :focus
-  describe 'correctly handles Markdown with embedded HTML tag pairs' do
-    describe 'when converting the lot to HTML' do
-      let(:mixed_source) do
-        'This is <a id="foo-begin"></a>*emphasised content*<a id="foo-end">' \
-          '</a> and regular content.'
-      end
-      let(:equivalent_html) do
-        '<p>This is <a id="foo-begin"></a><em>emphasised content</em>' \
-          '<a id="foo-end"></a> and regular content.</p>'
-      end
-      let(:converted_html) do
-        PandocRuby.convert(mixed_source, from: :markdown_github, to: :html)
+    # This is true for `pandoc-ruby` 2.0.0 on Linux. That Gem talking to Pandoc
+    # under Mac OS X does not *require* "fiddling" except to add the `a href=""`
+    # attributes to the anchor tag.
+    describe 'only on Linux' do
+      it 'removes the identified anchor pair from the converted Markdown' do
+        strips_anchors = markdown.match(/contrib/)
+        expect(strips_anchors).must_be(:nil?) if is_linux
+        expect(strips_anchors).wont_be(:nil?) if is_osx
       end
 
-      it 'preserving the embedded HTML' do
-        expect(converted_html.chomp).must_equal equivalent_html
+      it 'omits the identified anchor pair from the converted Markdown' do
+        stripped = html_content.sub('<a id="contrib-99"></a>', '')
+        expect(converted_html).must_equal stripped if is_linux
+        expect(converted_html).wont_equal stripped if is_osx
       end
-    end
-  end # describe 'correctly handles Markdown with embedded HTML tag pairs'
+    end # describe 'only on Linux'
+
+    describe 'on either Linux or OS X' do
+      it 'does not produce lexically identical content' do
+        expect(converted_html).wont_equal html_content
+      end
+
+      it 'correctly converts the "fiddled" content => Markdown => HTML' do
+        fiddled = fiddled_html.gsub('<a id', '<a href="" id')
+        expect(fiddled_html).must_equal fiddled
+      end
+    end # describe 'on either Linux or OS X'
+  end # describe 'has OS-dependent parsing of identified anchor pairs'
 end
